c1a0f76a5ba873ba138883b6a249e75adc80720a
mertbayramusta <mertb_99@hotmail.com>
2021-05-30 12:22:31 +0300

    home page shows the table that has the mock data

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import {NgxDatatableModule} from '@swimlane/ngx-datatable';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NgxDatatableModule,
   ],
   declarations: [HomePage]
 })
